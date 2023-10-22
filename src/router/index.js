d722d34a40a99665374b37babab94ae069b33d2e
MD.Taraq Rahman <taraqr9@gmail.com>
2023-10-22 08:34:39 +0600

    Project bootstrapped

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '../views/Index.vue'
+import About from '../views/About.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+  ]
+})
+
+export default router
