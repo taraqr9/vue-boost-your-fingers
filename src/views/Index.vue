adafb016ac221dfd5cd948d19dd6595aaf8084f1
Taraq Rahman <taraqr9@gmail.com>
2023-11-09 18:06:38 +0600

    Completed restart and show result

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -7,9 +7,12 @@ const user_input_number = ref(0);
 const user_input = ref('');
 const matched = ref(0);
 const not_matched = ref(0);
-const time = ref(5);
+const time = ref(20);
 const count_down_start = ref(false);
-const intervalId = ref(null);
+const interval_id = ref(null);
+const property_readonly = ref(false);
+const show_result = ref(false);
+
 
 function checkWord() {
   if (user_input.value.trim().length > 0) {
@@ -29,21 +32,29 @@ function checkWord() {
 function startCountDownIfNotStart() {
   if (!count_down_start.value) {
     count_down_start.value = true;
-    intervalId.value = setInterval(() => {
+    interval_id.value = setInterval(() => {
       if (time.value > 0) {
         time.value--;
       } else {
+        property_readonly.value = true;
         user_input.value = '';
-        clearInterval(intervalId);
+        clearInterval(interval_id.value);
+        show_result.value = true;
       }
     }, 1000);
   }
 }
 
-function restart(){
-  time.value = 60;
+function restart() {
+  time.value = 20;
+  property_readonly.value = false;
   count_down_start.value = false;
-  this.input.removeAttribute('readonly');
+  user_input.value = '';
+  matched.value = 0;
+  not_matched.value = 0;
+  user_input_number.value = 0;
+  show_result.value = false;
+  clearInterval(interval_id.value);
 }
 
 </script>
@@ -53,19 +64,16 @@ function restart(){
 
   <div class="h-screen flex items-center justify-center bg-gray-400">
     <div
-        class="w-2/5 p-8 bg-white rounded-xl shadow-2xl h-full"
+        class="w-1/5 p-8 bg-white rounded-xl shadow-2xl h-full"
     >
       <h3 class="text-center text-2xl mb-2 bg-green-500 rounded-full p-2 text-white">
         Finger Booster
       </h3>
 
-      <label
-          for="message"
-          class="block mb-2 text-sm font-medium text-gray-900">Your message</label>
+      <label class="block mb-2 text-sm font-medium text-gray-900">Your message</label>
       <div
-          id="message"
-          class="block p-2.5 w-full h-48 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-      >
+          class="block p-2.5 w-full h-48 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 overflow-auto"
+          style="user-select: none;">
 
         <span v-for="(pera, index) in arrayPera" :key="index"
               :class="index === user_input_number ? 'bg-gray-300 text-black rounded-md' : ''">
@@ -75,7 +83,7 @@ function restart(){
       </div>
 
       <div class="flex flex-row my-6">
-        <div class="basis-2/4 m-1">
+        <div class="basis-3/4 m-1">
           <label
               for="large-input"
               class="block mb-2 text-sm font-medium text-gray-900">Start Typing</label>
@@ -85,14 +93,15 @@ function restart(){
               @keydown="startCountDownIfNotStart"
               @keyup.space="checkWord"
               v-model="user_input"
-              :readonly="time === 0"
+              :readonly="property_readonly"
+              placeholder="Type here..."
           />
         </div>
 
         <div class="basis-1/4 m-1">
           <label
               for="large-input"
-              class="block mb-2 text-sm font-medium text-white bg-green-500 rounded text-center">Time</label>
+              class="block mb-2 text-sm font-medium">Time</label>
           <input
               type="text"
               class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
@@ -100,41 +109,41 @@ function restart(){
               readonly
           />
         </div>
-
-        <div class="basis-1/4">
-          <label
-              for="large-input"
-              class="block mb-2 text-sm font-medium text-white bg-green-500 rounded text-center"></label>
-          <button class="w-full h-10 mt-8 text-white bg-green-500 rounded" @click="restart">
-            Restart
-          </button>
-        </div>
       </div>
 
+      <div class="w-full flex items-center justify-end">
+        <button class="w-20 h-10 text-white bg-green-500 rounded" @click="restart">
+          Restart
+        </button>
+      </div>
 
-      <div class="flex flex-row p-2">
-        <div class="basis-2/4 m-1">
-          <label
-              for="large-input"
-              class="block mb-2 text-sm font-medium text-white bg-green-500 rounded text-center">Matched</label>
-          <input
-              type="text"
-              class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
-              :value="matched"
-              readonly
-          />
+      <div v-show="show_result">
+        <div class="px-4 sm:px-0 flex justify-center mt-2">
+          <h3 class="text-base font-semibold leading-7 text-gray-900 border-2 border-b-green-400 w-48 text-center rounded-2xl p-2 shadow-lg">
+            Result
+          </h3>
         </div>
+        <div class="mt-4 h-80 border-2 rounded-xl p-4">
+          <div class="flex items-center justify-center">
+            <img class="shadow-lg ring-1 w-20 ring-inset ring-green-300 hover:bg-gray-50 rounded-xl"
+                 src="../assets/typing.png" alt="weather"/>
+          </div>
 
-        <div class="basis-2/4 m-1">
-          <label
-              for="large-input"
-              class="block mb-2 text-sm font-medium text-white bg-rose-500 rounded text-center">Not Matched</label>
-          <input
-              type="text"
-              class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
-              :value="not_matched"
-              readonly
-          />
+          <div class=" mt-10">
+            <div class="flex m-4">
+              <div class="w-3/4 text-sm font-medium text-gray-900">Correct words</div>
+              <div class="mt-1 text-sm text-green-500 sm:col-span-2 sm:mt-0">
+                {{ matched }}
+              </div>
+            </div>
+            <hr>
+            <div class="flex m-4">
+              <div class="w-3/4 text-sm font-medium text-gray-900">Wrong words</div>
+              <div class="mt-1 text-sm text-rose-500 sm:col-span-2 sm:mt-0">
+                {{ not_matched }}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
